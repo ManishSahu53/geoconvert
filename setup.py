22f731d4881dc8c2fe0ae7387c82dfa2eff4a27e
Manish Sahu <manishsahu@Manishs-MacBook-Air.local>
2019-04-02 19:05:51 +0530

    removed readme.md in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='geoconvert',
-    version='0.1.0',
+    version='0.1.1',
     author='Manish Sahu',
     author_email='manish@indshine.com',
     url="https://gitlab.com/manish.indshine/geoconvert.git",
